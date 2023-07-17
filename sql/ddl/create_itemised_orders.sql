5511dda372b7e8ac42c4349de7b6e7fb165532f7
m01nak <m01nak77@gmail.com>
2023-07-18 02:07:29 +0530

    break orders into orders and itemised_orders

---
+++ sql/ddl/create_itemised_orders.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------
+-- Creating table "itemised_orders" in "main" schema the database --
+--------------------------------------------------------------------
+
+create table main.itemised_orders (
+	order_id int not null auto_increment,
+    product_id int not null,
+    product_count int not null,
+    primary key (order_id,product_id),
+    foreign key(order_id) references orders(order_id),
+    foreign key(product_id) references products(product_id)
+);
--- sql/ddl/orders.sql
+++ sql/ddl/orders.sql
@@ -8,10 +8,7 @@ create table main.orders (
     order_created_dtm timestamp not null default now(),
     order_closed_dtm timestamp,
     order_status varchar(20) not null default 'new',
-    order_product_id int not null,
-    order_product_count int not null,
     primary key(order_id),
     foreign key(user_id) references users(user_id),
-    foreign key(order_status) references order_statuses(order_status),
-    foreign key(order_product_id) references products(product_id)
+    foreign key(order_status) references order_statuses(order_status)
 );
